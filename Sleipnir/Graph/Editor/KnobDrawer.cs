5ebd37f39a61883d1c2d5f4261e39569f42a3599
Michał Pogoda-Kotwica <michalpogodakotwica@gmail.com>
2018-09-09 13:30:58 +0200

    Connection drawing bug fix.

--- Sleipnir/Graph/Editor/KnobDrawer.cs
+++ Sleipnir/Graph/Editor/KnobDrawer.cs
@@ -18,8 +18,10 @@ namespace Sleipnir.Graph.Editor
             var knobs = ((BaseNode)Property.Parent.Parent.ValueEntry.WeakSmartValue).GetKnobs(Property.Name);
             var rect = EditorGUILayout.GetControlRect(false, 0);
             var yPosition = rect.y + EditorGUIUtility.singleLineHeight;
-            foreach (var knob in knobs)
-                knob.RelativeYPosition = yPosition + Node.HeaderHeight;
+            if (Event.current.type == EventType.Repaint)
+                foreach (var knob in knobs)
+                    knob.RelativeYPosition = yPosition + Node.HeaderHeight;
+
             CallNextDrawer(label);
         }
     }
